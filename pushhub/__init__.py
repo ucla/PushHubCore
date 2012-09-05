a4972e8b0e187e80ad643bf0d53a1e2930da1610
Nolan Brubaker <nolan@nbrubaker.com>
2012-09-05 16:39:52 +0000

    Add hub utility configuration.

--- pushhub/__init__.py
+++ pushhub/__init__.py
@@ -3,17 +3,21 @@ from pyramid_zodbconn import get_connection
 
 from .views import publish
 
+
 def root_factory(request):
     conn = get_connection(request)
     return appmaker(conn.root())
 
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    # XXX: The Hub will need to initialize here, most likely.
-    # perhaps it can be attached to the request, so it's accessible from
-    # all the views?
     config = Configurator(root_factory=root_factory, settings=settings)
+
+    # Insert the hub in to the app-global registry as a singleton utility.
+    config.hook_zca()
+    config.include('.hub.configure_hub')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.add_route('publish', '/publish')
--- pushhub/hub.py
+++ pushhub/hub.py
@@ -5,8 +5,32 @@ The hub itself does not have state, but does hold references to
 subscribers and topics.
 """
 
+from zope.component import provideUtility
+from zope.interface import Interface, implements
+
+
+def configure_hub(hub_impl=None):
+    """Registers a hub implementation logic as a ZCA utility.
+
+    This should be called by the application Configurator in
+    order to register the Hub at initilization.
+
+    An alternative implementation can be provided, mostly for
+    testing purposes.
+    """
+    if not hub_impl:
+        provideUtility(Hub(), provides=IHub)
+    provideUtility(hub_impl(), provides=IHub)
+
+
+class IHub(Interface):
+    """Marker interface for hub implementations"""
+    pass
+
 
 class Hub(object):
+    implements(IHub)
+
     def __init__(self):
         # Should load the subscribsers/topics out of the DB here.
         pass
