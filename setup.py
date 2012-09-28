65d8ecd6fdbb6f93c013d71b6c1cf232d861b3b5
Clayton Parker <me@claytron.com>
2012-09-28 19:37:14 +0000

    Prepare push-hub 0.5.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = [
     ]
 
 setup(name='push-hub',
-      version='0.4',
+      version='0.5',
       description='push-hub',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
